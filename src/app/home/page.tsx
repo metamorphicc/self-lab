7294c9456e30ff90888b0126e81c7e5a2fd2c7ad
sergey <artemkruglikov370@gmail.com>
2026-01-25 16:43:57 +0700

    first steps

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -8,11 +8,28 @@ export default function Home({}) {
   const [isClose, setIsClose] = useState(true);
   return (
     <>
-        <ModalWindow
+      <div className="h-screen w-screen">
+        <div className="px-7     flex items-start justify-between">
+          <div className="shadow-xs h-screen bg-black w-[300px]">
+            <div className="flex items-center gap-3 justify-center flex-col mt-3">
+                <h1>Morph</h1> <span>1lvl</span>
+            </div>
+            <div>
+                <ul>
+                    <li>
+                        
+                    </li>
+                </ul>
+            </div>
+          </div>
+          <div className="shadow-xs bg-black">fds</div>
+          <div className="shadow-xs bg-black">fds</div>
+        </div>
+      </div>
+      {/* <ModalWindow
           isOpen={isClose}
           onClose={() => setIsClose(false)}
-        ></ModalWindow>
-
+        ></ModalWindow> */}
     </>
   );
 }
